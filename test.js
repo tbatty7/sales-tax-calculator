d4e3d4cde1bffd8dbde1eb851045420a6397ce2a
Timothy Batty <tbatty7@gmail.com>
2017-11-10 22:46:59 -0500

    First test passed, valid module created

---
+++ test.js
@@ -0,0 +1,9 @@
+const expect = require('chai').expect;
+
+describe('Sales Tax Calculator', ()=>{
+	it('Should have a valid module', ()=>{
+		const Calculator = require('./calculator');
+	});
+
+
+});
