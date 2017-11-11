8a3a3a80bba53b2f14d151d38b608b0092f782dc
Timothy Batty <tbatty7@gmail.com>
2017-11-11 10:06:45 -0500

    Passed test to calculate sales tax and add it as a seperate value to itemList

--- calculator.js
+++ calculator.js
@@ -1,5 +1,12 @@
 var Calculator = function(){
 	this.itemList = [];
+	this.exempt = {
+		'book': true,
+		'chocolate bar': true,
+		'imported box of chocolates': true,
+		'box of imported chocolates': true,
+		'packet of headache pills': true,
+	};
 };
 
 // The clear function clears the itemList.
@@ -22,6 +29,7 @@ Calculator.prototype.parseInput = function(str){
 		itemName: itemName,
 		quantity: quantity,
 		price: price,
+		salesTax: 0,
 	};
 };
 
@@ -29,20 +37,34 @@ Calculator.prototype.parseInput = function(str){
 // and pushes it into the itemList array.
 Calculator.prototype.inputItem = function(str){
 	this.itemList.push(this.parseInput(str));
-}
+};
 
 // The checkout function should take the itemList array and format it back into
 // a string and print it out.
 Calculator.prototype.printLine = function(index){
 	return this.itemList[index].quantity + ' ' + this.itemList[index].itemName + ': ' + this.itemList[index].price / 100;
-}
+};
 
+// The printTotal function takes the price from each item in itemList and adds them together.
+// It then converts the money back to a decimal and prints it in a string of "Total: amount"
 Calculator.prototype.printTotal = function(){
 	var total = 0;
 	this.itemList.forEach(function(item){
 		total += item.price;
 	});
 	return "Total: " + total/100;
-}
+};
+
+Calculator.prototype.calculateSalesTax = function(){
+	var exemptList = this.exempt;
+	this.itemList.forEach(function(item){
+		if (exemptList[item.itemName]){
+			return;
+		} else {
+			item.salesTax += Math.ceil((item.price / 10) / 5)*5;
+			}
+	});
+};
+	
 
 module.exports = new Calculator();
--- test.js
+++ test.js
@@ -4,39 +4,47 @@ describe('Sales Tax Calculator', ()=>{
 	it('Should have a valid module', ()=>{
 		const Calculator = require('./calculator');
 
+		beforeEach(()=>{
+			return Calculator.clear();
+		});
+
 		describe('Tests for List of Items', ()=>{
 			it('Should have an array named itemList', ()=>{
 				expect(Calculator.itemList).to.be.an('array');
 			});
 
-			it('Function parseInput() should turn input string into an object', ()=>{
-				Calculator.clear();
-				expect(Calculator.parseInput('1 book at 12.49')).to.deep.equal({'itemName': 'book', 'quantity': 1, 'price': 1249});
+			it('should turn input string into an object', ()=>{
+				expect(Calculator.parseInput('1 book at 12.49')).to.deep.equal({'itemName': 'book', 'quantity': 1, 'price': 1249, 'salesTax': 0});
 			});
 
-			it('Function inputItem() should add item to itemList', ()=>{
-				Calculator.clear();
+			it('should add item to itemList', ()=>{
 				Calculator.inputItem('1 book at 12.49');
-				expect(Calculator.itemList).to.deep.include({'itemName': 'book', 'quantity': 1, 'price': 1249});
+				expect(Calculator.itemList).to.deep.include({'itemName': 'book', 'quantity': 1, 'price': 1249, 'salesTax': 0});
 			});
 
 		});
 
 		describe('Tests for output', ()=>{
-			it('Function printLine() should return at least 1 item formatted with price and quantity', ()=>{
-				Calculator.clear();
+			it('should return at least 1 item formatted with price and quantity', ()=>{
 				Calculator.inputItem('1 book at 12.49');
 				expect(Calculator.printLine(0)).to.equal('1 book: 12.49');
 			});
 			
-			it('Function printTotal() should return the total of the cost of the items', ()=>{
-				Calculator.clear();
+			it('should return the total of the cost of the items', ()=>{
 				Calculator.inputItem('1 book at 12.49');
 				Calculator.inputItem('1 chocolate bar at 0.85');
 				expect(Calculator.printTotal()).to.equal('Total: 13.34');
 			});
 
 		});
+
+		describe('Tests for calculating sales tax', ()=>{
+			it('Function calculateSalesTax() should add sales tax and hold it as a separate variable', ()=>{
+				Calculator.inputItem('1 music CD at 14.99');
+				Calculator.calculateSalesTax();
+				expect(Calculator.itemList).to.deep.include({'itemName': 'music CD', 'quantity': 1, 'price': 1499, 'salesTax': 150});
+			})
+		})
 	});
 
 });
